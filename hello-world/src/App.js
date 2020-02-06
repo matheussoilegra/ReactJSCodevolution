ec71c453ef208007be9a72c7b85bb7e6f4ee6ce0
Matheus Santos <matheus.santos@ilegra.com>
2020-02-06 14:27:25 -0300

    Added Hello component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet></Greet>
-        <Welcome></Welcome>
+        {/* <Greet></Greet> */}
+        {/* <Welcome></Welcome> */}
+        <Hello></Hello>
       </div>
     );
   }
