dd937331fb2cdb6ca37512acdebf31fd3aa80371
Matheus Santos <matheus.santos@ilegra.com>
2020-02-07 12:01:24 -0300

    Added binding event handlers

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,13 +7,15 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick></FunctionClick>
-        <ClassClick></ClassClick>
+        <EventBind></EventBind>
+        {/* <FunctionClick></FunctionClick> */}
+        {/* <ClassClick></ClassClick> */}
         {/* <Counter></Counter> */}
         {/* <Message></Message> */}
         {/* <Greet name="Bruce" heroName="Batman">
