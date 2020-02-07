d7e1794a30a1b5f23e9a1dac5e824597ebef96d0
Matheus Santos <matheus.santos@ilegra.com>
2020-02-07 10:07:15 -0300

    Added setState

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,13 +4,15 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message></Message>
-        {/* <Greet name="Bruce" heroName="Batman">
+        <Counter></Counter>
+        {/*<Message></Message>
+        <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
