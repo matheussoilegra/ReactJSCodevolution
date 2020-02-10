7073e53e4a2eafc6330ec74212906bf4cbd921f1
Matheus Santos <matheus.santos@ilegra.com>
2020-02-10 14:37:25 -0300

    Added components mounting lifecycle methods

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -16,12 +16,14 @@ import Inline from './components/Inline';
 import './appStyles.css';
 import styles from './appStyles.module.css';
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form></Form>
+        <LifecycleA></LifecycleA>
+        {/* <Form></Form> */}
         {/* <h1 className="error">Error</h1> */}
         {/* <h1 className={styles.sucess}>Sucess</h1> */}
         {/* <Inline></Inline> */}
