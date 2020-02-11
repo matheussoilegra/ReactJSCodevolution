d94c807c8dcff7900a583173649097ede61ad60b
Matheus Santos <matheus.santos@ilegra.com>
2020-02-11 09:21:03 -0300

    Added Fragments

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -17,12 +17,16 @@ import './appStyles.css';
 import styles from './appStyles.module.css';
 import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LifecycleA></LifecycleA>
+        <Table></Table>
+        {/* <FragmentDemo></FragmentDemo> */}
+        {/* <LifecycleA></LifecycleA> */}
         {/* <Form></Form> */}
         {/* <h1 className="error">Error</h1> */}
         {/* <h1 className={styles.sucess}>Sucess</h1> */}
---
+++ my-project/src/components/Columns.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Columns() {
+  return (
+    // <React.Fragment>
+    //   <td>Name</td>
+    //   <td>Matheus</td>
+    // </React.Fragment>
+    <>
+      <td>Name</td>
+      <td>Matheus</td>
+    </>
+  );
+}
+
+export default Columns;
