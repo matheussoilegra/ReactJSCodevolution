84345e086b7dc1e9af1d9e108fea678d65a412fb
Matheus Santos <matheus.santos@ilegra.com>
2020-02-11 10:15:39 -0300

    Added Pure Component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -19,12 +19,16 @@ import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Table></Table>
+        {/* <ParentComp></ParentComp> */}
+        {/* <PureComp></PureComp> */}
+        {/* <Table></Table> */}
         {/* <FragmentDemo></FragmentDemo> */}
         {/* <LifecycleA></LifecycleA> */}
         {/* <Form></Form> */}
