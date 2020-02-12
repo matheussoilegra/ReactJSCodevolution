78b5c9678797ab19064d65e9de926597b9c77836
Matheus Santos <matheus.santos@ilegra.com>
2020-02-12 11:55:22 -0300

    Added Higher Order Components

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounter></ClickCounter>
+        <ClickCounter name="Matheus"></ClickCounter>
         <HoverCounter></HoverCounter>
         {/* <ErrorBoundary>
           <Hero heroName="Batman"></Hero>
